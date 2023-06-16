a8a7d2e10cf182a4ab8cab7aa803fc7404094ef7
Asenszyn <92269640+Asenszyn@users.noreply.github.com>
2023-06-16 12:07:01 +0200

    Update bazadamych.py

--- bazadamych.py
+++ bazadamych.py
@@ -167,33 +167,27 @@ def utworz_tabele():
 def wypelnij_tabele():
     conn = sqlite3.connect('baza_danych.db')
     c = conn.cursor()
+    
+# Wypełnienie tabel danymi
+c.execute("INSERT INTO tabela1 (id, nazwa, cena) VALUES (1, 'Produkt 1', 10.99)")
+c.execute("INSERT INTO tabela1 (id, nazwa, cena) VALUES (2, 'Produkt 2', 15.99)")
+c.execute("INSERT INTO tabela1 (id, nazwa, cena) VALUES (3, 'Produkt 3', 20.50)")
 
-    # Tabela 1
-    c.execute("INSERT INTO tabela1 (kolumna1, kolumna2) VALUES ('Akcesorium 1', 'Kategoria A')")
-    c.execute("INSERT INTO tabela1 (kolumna1, kolumna2) VALUES ('Akcesorium 2', 'Kategoria B')")
-    c.execute("INSERT INTO tabela1 (kolumna1, kolumna2) VALUES ('Akcesorium 3', 'Kategoria A')")
-    c.execute("INSERT INTO tabela1 (kolumna1, kolumna2) VALUES ('Akcesorium 4', 'Kategoria C')")
+c.execute("INSERT INTO tabela2 (id, marka, ilosc) VALUES (1, 'Marka A', 50)")
+c.execute("INSERT INTO tabela2 (id, marka, ilosc) VALUES (2, 'Marka B', 100)")
+c.execute("INSERT INTO tabela2 (id, marka, ilosc) VALUES (3, 'Marka C', 75)")
 
-    # Tabela 2
-    c.execute("INSERT INTO tabela2 (kolumna1, kolumna2) VALUES ('Akcesorium 5', 'Kategoria B')")
-    c.execute("INSERT INTO tabela2 (kolumna1, kolumna2) VALUES ('Akcesorium 6', 'Kategoria A')")
-    c.execute("INSERT INTO tabela2 (kolumna1, kolumna2) VALUES ('Akcesorium 7', 'Kategoria C')")
-    c.execute("INSERT INTO tabela2 (kolumna1, kolumna2) VALUES ('Akcesorium 8', 'Kategoria B')")
+c.execute("INSERT INTO tabela3 (id, kategoria, opis) VALUES (1, 'Kategoria A', 'Opis kategorii A')")
+c.execute("INSERT INTO tabela3 (id, kategoria, opis) VALUES (2, 'Kategoria B', 'Opis kategorii B')")
+c.execute("INSERT INTO tabela3 (id, kategoria, opis) VALUES (3, 'Kategoria C', 'Opis kategorii C')")
 
-    # Tabela 3
-    c.execute("INSERT INTO tabela3 (kolumna1, kolumna2) VALUES ('Akcesorium 9', 'Kategoria C')")
-    c.execute("INSERT INTO tabela3 (kolumna1, kolumna2) VALUES ('Akcesorium 10', 'Kategoria B')")
-    c.execute("INSERT INTO tabela3 (kolumna1, kolumna2) VALUES ('Akcesorium 11', 'Kategoria A')")
-    c.execute("INSERT INTO tabela3 (kolumna1, kolumna2) VALUES ('Akcesorium 12', 'Kategoria C')")
+c.execute("INSERT INTO tabela4 (id, producent, rok) VALUES (1, 'Producent X', 2019)")
+c.execute("INSERT INTO tabela4 (id, producent, rok) VALUES (2, 'Producent Y', 2020)")
+c.execute("INSERT INTO tabela4 (id, producent, rok) VALUES (3, 'Producent Z', 2021)")
 
-    # T# Tabela 4
-    c.execute("INSERT INTO tabela4 (kolumna1, kolumna2) VALUES ('Akcesorium 13', 'Kategoria A')")
-    c.execute("INSERT INTO tabela4 (kolumna1, kolumna2) VALUES ('Akcesorium 14', 'Kategoria C')")
-    c.execute("INSERT INTO tabela4 (kolumna1, kolumna2) VALUES ('Akcesorium 15', 'Kategoria B')")
-    c.execute("INSERT INTO tabela4 (kolumna1, kolumna2) VALUES ('Akcesorium 16', 'Kategoria A')")
-
-    conn.commit()
-    conn.close()
+# Zatwierdzenie zmian i zamknięcie połączenia
+conn.commit()
+conn.close()
 
 # Funkcja sortująca rekordy w bazie danych według kategorii dla każdej tabeli
 def sortuj_rekordy_kategoria():
