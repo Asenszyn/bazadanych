25b792d1e384770e3d13600704807d3cb76206da
Asenszyn <92269640+Asenszyn@users.noreply.github.com>
2023-06-16 12:02:31 +0200

    Update bazadanych sql.py

--- bazadanych sql.py
+++ bazadanych sql.py
@@ -6,45 +6,41 @@ c = conn.cursor()
 
 # Utworzenie tabel
 c.execute('''CREATE TABLE IF NOT EXISTS tabela1
-             (id INTEGER PRIMARY KEY AUTOINCREMENT,
-             kolumna1 TEXT,
-             kolumna2 TEXT)''')
+             (id INTEGER PRIMARY KEY,
+             nazwa TEXT,
+             cena REAL)''')
 
 c.execute('''CREATE TABLE IF NOT EXISTS tabela2
-             (id INTEGER PRIMARY KEY AUTOINCREMENT,
-             kolumna1 TEXT,
-             kolumna2 TEXT)''')
+             (id INTEGER PRIMARY KEY,
+             marka TEXT,
+             ilosc INTEGER)''')
 
 c.execute('''CREATE TABLE IF NOT EXISTS tabela3
-             (id INTEGER PRIMARY KEY AUTOINCREMENT,
-             kolumna1 TEXT,
-             kolumna2 TEXT)''')
+             (id INTEGER PRIMARY KEY,
+             kategoria TEXT,
+             opis TEXT)''')
 
 c.execute('''CREATE TABLE IF NOT EXISTS tabela4
-             (id INTEGER PRIMARY KEY AUTOINCREMENT,
-             kolumna1 TEXT,
-             kolumna2 TEXT)''')
+             (id INTEGER PRIMARY KEY,
+             producent TEXT,
+             rok INTEGER)''')
 
 # Wypełnienie tabel danymi
-c.execute("INSERT INTO tabela1 (kolumna1, kolumna2) VALUES ('Akcesorium 1', 'Kategoria A')")
-c.execute("INSERT INTO tabela1 (kolumna1, kolumna2) VALUES ('Akcesorium 2', 'Kategoria B')")
-c.execute("INSERT INTO tabela1 (kolumna1, kolumna2) VALUES ('Akcesorium 3', 'Kategoria A')")
-c.execute("INSERT INTO tabela1 (kolumna1, kolumna2) VALUES ('Akcesorium 4', 'Kategoria C')")
+c.execute("INSERT INTO tabela1 (id, nazwa, cena) VALUES (1, 'Produkt 1', 10.99)")
+c.execute("INSERT INTO tabela1 (id, nazwa, cena) VALUES (2, 'Produkt 2', 15.99)")
+c.execute("INSERT INTO tabela1 (id, nazwa, cena) VALUES (3, 'Produkt 3', 20.50)")
 
-c.execute("INSERT INTO tabela2 (kolumna1, kolumna2) VALUES ('Akcesorium 5', 'Kategoria B')")
-c.execute("INSERT INTO tabela2 (kolumna1, kolumna2) VALUES ('Akcesorium 6', 'Kategoria A')")
-c.execute("INSERT INTO tabela2 (kolumna1, kolumna2) VALUES ('Akcesorium 7', 'Kategoria C')")
-c.execute("INSERT INTO tabela2 (kolumna1, kolumna2) VALUES ('Akcesorium 8', 'Kategoria B')")
+c.execute("INSERT INTO tabela2 (id, marka, ilosc) VALUES (1, 'Marka A', 50)")
+c.execute("INSERT INTO tabela2 (id, marka, ilosc) VALUES (2, 'Marka B', 100)")
+c.execute("INSERT INTO tabela2 (id, marka, ilosc) VALUES (3, 'Marka C', 75)")
 
-c.execute("INSERT INTO tabela3 (kolumna1, kolumna2) VALUES ('Akcesorium 9', 'Kategoria C')")
-c.execute("INSERT INTO tabela3 (kolumna1, kolumna2) VALUES ('Akcesorium 10', 'Kategoria B')")
-c.execute("INSERT INTO tabela3 (kolumna1, kolumna2) VALUES ('Akcesorium 11', 'Kategoria A')")
-c.execute("INSERT INTO tabela3 (kolumna1, kolumna2) VALUES ('Akcesorium 12', 'Kategoria C')")
+c.execute("INSERT INTO tabela3 (id, kategoria, opis) VALUES (1, 'Kategoria A', 'Opis kategorii A')")
+c.execute("INSERT INTO tabela3 (id, kategoria, opis) VALUES (2, 'Kategoria B', 'Opis kategorii B')")
+c.execute("INSERT INTO tabela3 (id, kategoria, opis) VALUES (3, 'Kategoria C', 'Opis kategorii C')")
 
-c.execute("INSERT INTO tabela4 (kolumna1, kolumna2) VALUES ('Akcesorium 13', 'Kategoria A')")
-c.execute("INSERT INTO tabela4 (kolumna1, kolumna2) VALUES ('Akcesorium 14', 'Kategoria C')")
-c.execute("INSERT INTO tabela4 (kolumna1, kolumna2) VALUES ('Akcesorium 15', 'Kategoria B')")
-c.execute("INSERT INTO tabela4 (kolumna1, kolumna2) VALUES ('Akcesorium 16', 'Kategoria A')")
+c.execute("INSERT INTO tabela4 (id, producent, rok) VALUES (1, 'Producent X', 2019)")
+c.execute("INSERT INTO tabela4 (id, producent, rok) VALUES (2, 'Producent Y', 2020)")
+c.execute("INSERT INTO tabela4 (id, producent, rok) VALUES (3, 'Producent Z', 2021)")
 
 # Zatwierdzenie zmian i zamknięcie połączenia
 conn.commit()
